8dc90fdfb698f225bb30254cbb5ba0ba4f498c37
Richard Felix <rfelixmail@gmail.com>
2016-06-12 03:04:22 -0400

    refactor: preliminary work on fixing margins for rows with pictures and words. That are side by side. Also took out some unessary margin classes from generalScope.scss

--- public/js/custom.js
+++ public/js/custom.js
@@ -16,9 +16,44 @@ $(function() {
 /************************************************
     Collapse menu on click (only for mobile)
 ************************************************/
-  $(function(){
-    $(".navbar-collapse a").click(function(){
-      $(".navbar-collapse").removeClass("in");
-      $(".navbar-collapse").css({'height': '0px'});
-    });
-  }());
+$(function(){
+  $(".navbar-collapse a").click(function(){
+    $(".navbar-collapse").removeClass("in");
+    $(".navbar-collapse").css({'height': '0px'});
+  });
+}());
+
+/************************************************
+    Fixing margins of col-md-6 boxes to match
+************************************************/
+var picArr = [ '.pic', '.pic1', '.pic2' ],
+    wordArr = [ '.words', 'words1', '.words2' ],
+    wordMarginArr = [];
+
+for(var x = 0; x < picArr.length; x++){
+  var result = ( $(picArr[x]).height() - $(wordArr[x]).height() ) / 2.1;
+  wordMarginArr.push( result );
+}
+
+console.log(wordMarginArr);
+
+function fixMargins(){
+  var wordMarginArrLength = wordMarginArr.length;
+
+  for(var x = 0; x < wordMarginArrLength; x++){
+    if( wordMarginArr[x] > 0 ){
+      $(wordArr[x]).css({
+        'margin-top': wordMarginArr[x],
+        'margin-bottom': wordMarginArr[x]
+      });
+    }
+  }
+}
+
+$(document).ready(function(){
+  fixMargins();
+});
+
+$(window).on('resize', function() {
+  fixMargins();
+});
